cc9b45fae14970ed2afcba8a3c4963b20f57affd
Anastasiia Leusenko <anastasileusenko@icloud.com>
2026-07-17 14:18:57 +0300

    Типи данних,калькулятор чайових

---
+++ tip_calculator.py
@@ -0,0 +1,8 @@
+bill = 457.0
+tip_percent = 15
+tip_amount = bill * tip_percent / 100
+total = bill + tip_amount
+
+print(str("Рахунок:"), bill)
+print(str("Чайові:"), tip_amount)
+print(str("Разом до сплати:"), total)
---
+++ types.py
@@ -0,0 +1,36 @@
+# Вправа 1
+temperature = 20
+deep = 2.3
+pet = "Кішка Василіса"
+good_job = True
+
+print(type(temperature))
+print(type(deep))
+print(type(pet))
+print(type(good_job))
+
+# Вправа 2
+# print(10 + "10")
+
+# Вправа 3
+print(str(10) + "10")
+
+# Вправа 4
+print(10 + int("10"))
+
+# Вправа 5
+print(5 > 3)
+print(5 == 5)
+print(5 == "5")
+print(10 != 5)
+
+# Вправа 6
+print(True + True)
+print(False + 5)
+print(True * 10)
+
+# Вправа 7
+print(bool(0))
+print(bool(1))
+print(bool(""))
+print(bool("привіт"))
